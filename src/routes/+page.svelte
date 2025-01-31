9bccf406c432a72304b6f4a0c7d8492fd603d2c9
Begad666 <47504169+Begad666@users.noreply.github.com>
2025-01-31 11:40:13 +0300

    test 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { PUBLIC_ENVIRONMENT_VARIABLE } from "$env/static/public";
 </script>
-This is an SSR page. Check the DevTools for no error.
+This is a pre-rendered page. Check the DevTools for an error.
 <br />
 Environment variable test: "{PUBLIC_ENVIRONMENT_VARIABLE}"
 <br />
-<a href="/pre-rendered">Pre-rendered page</a>
+<a href="/ssr">SSR page</a>
---
+++ src/routes/ssr/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { env } from "$env/dynamic/public";
+</script>
+This is an SSR page. Check the DevTools for no error.
+<br />
+Environment variable test: "{env.PUBLIC_ENVIRONMENT_VARIABLE}""
+<br />
+<a href="/">Pre-rendered page</a>
